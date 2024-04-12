205e818db22e0ece5e10c6c4e67b6adda0588aed
Chapalax <max250903@gmail.com>
2024-04-12 10:57:38 +0300

    Refactoring db schema

--- migrations/scripts/10-04-2024-create-users-table.sql
+++ migrations/scripts/10-04-2024-create-users-table.sql
@@ -12,9 +12,9 @@ CREATE TABLE IF NOT EXISTS users
     father_name TEXT        NOT NULL,
     grade       TEXT        NOT NULL,
     birth_date  TIMESTAMP   NOT NULL,
-    email       TEXT        NOT NULL,
-    phone       VARCHAR(32) NOT NULL,
-    passport    TEXT        NOT NULL
+    email       TEXT        NOT NULL UNIQUE,
+    phone       VARCHAR(32) NOT NULL UNIQUE,
+    passport    TEXT        NOT NULL UNIQUE
 );
 
 ALTER SEQUENCE users_seq OWNED BY users.id;
